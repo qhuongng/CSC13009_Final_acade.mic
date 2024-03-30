0ed89791c66b2482b2b27ddfa7acf4e331c1c8f8
MinhDuc2412 <minhducc.bui@gmail.com>
2024-03-30 18:41:50 +0700

    Handle logic and finish function delete record

--- app/src/main/java/com/example/acade_mic/GalleryActivity.java
+++ app/src/main/java/com/example/acade_mic/GalleryActivity.java
@@ -1,11 +1,14 @@
 package com.example.acade_mic;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -41,9 +44,11 @@ public class GalleryActivity extends AppCompatActivity implements OnItemClickLis
     private View editbar;
     private ImageButton btnClose;
     private ImageButton btnSelectAll;
-
     private boolean allChecked = false;
-
+    private ImageButton btnRename;
+    private ImageButton btnDelete;
+    private TextView tvRename;
+    private TextView tvDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,6 +67,12 @@ public class GalleryActivity extends AppCompatActivity implements OnItemClickLis
                 onBackPressed();
             }
         });
+
+        btnRename = findViewById(R.id.btnEdit);
+        btnDelete = findViewById(R.id.btnDelete);
+        tvRename = findViewById(R.id.tvEdit);
+        tvDelete = findViewById(R.id.tvDelete);
+
 
         editbar = findViewById(R.id.editBar);
         btnClose = findViewById(R.id.btnClose);
@@ -127,8 +138,116 @@ public class GalleryActivity extends AppCompatActivity implements OnItemClickLis
                     rc.setChecked(allChecked);
                 }
                 mAdapter.notifyDataSetChanged();
+
+                if(allChecked){
+                    disableRename();
+                    enableDelete();
+                }else {
+                    disableRename();
+                    disableDelete();
+                }
             }
         });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(GalleryActivity.this);
+                builder.setTitle("Delete record?");
+                final int nbRecords = countSelectedRecords(records);
+                builder.setMessage("Are you sure you want to delete " + nbRecords + " record(s)?");
+
+                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ArrayList<AudioRecord> toDelete = new ArrayList<>();
+                        for (AudioRecord record : records) {
+                            if (record.isChecked()) {
+                                toDelete.add(record);
+                            }
+                        }
+
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                db.audioRecordDao().delete(toDelete);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        records.removeAll(toDelete);
+                                        mAdapter.notifyDataSetChanged();
+                                        leaveEditMode();
+                                    }
+                                });
+                            }
+                        }).start();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // it does nothing
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+            private int countSelectedRecords(ArrayList<AudioRecord> records) {
+                int count = 0;
+                for (AudioRecord record : records) {
+                    if (record.isChecked()) {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        });
+
+
+
+    }
+
+    private void leaveEditMode() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        editbar.setVisibility(View.GONE);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        for (AudioRecord record : records) {
+            record.setChecked(false);
+        }
+
+        mAdapter.setEditMode(false);
+    }
+
+    private void disableRename() {
+        btnRename.setClickable(false);
+        btnRename.setBackgroundTintList(ResourcesCompat.getColorStateList(getResources(), R.color.disabledDarkGray, getTheme()));
+        tvRename.setTextColor(ResourcesCompat.getColorStateList(getResources(), R.color.disabledDarkGray, getTheme()));
+    }
+
+    private void disableDelete() {
+        btnDelete.setClickable(false);
+        btnDelete.setBackgroundTintList(ResourcesCompat.getColorStateList(getResources(), R.color.disabledDarkGray, getTheme()));
+        tvDelete.setTextColor(ResourcesCompat.getColorStateList(getResources(), R.color.disabledDarkGray, getTheme()));
+    }
+
+    private void enableRename() {
+        btnRename.setClickable(true);
+        btnRename.setBackgroundTintList(ResourcesCompat.getColorStateList(getResources(), R.color.darkGray, getTheme()));
+        tvRename.setTextColor(ResourcesCompat.getColorStateList(getResources(), R.color.darkGray, getTheme()));
+    }
+
+    private void enableDelete() {
+        btnDelete.setClickable(true);
+        btnDelete.setBackgroundTintList(ResourcesCompat.getColorStateList(getResources(), R.color.darkGray, getTheme()));
+        tvDelete.setTextColor(ResourcesCompat.getColorStateList(getResources(), R.color.darkGray, getTheme()));
     }
 
     private void searchDatabase(String query) {
@@ -175,6 +294,28 @@ public class GalleryActivity extends AppCompatActivity implements OnItemClickLis
             if(mAdapter.isEditMode()){
                 records.get(position).setChecked(!records.get(position).isChecked());
                 mAdapter.notifyItemChanged(position);
+
+                int nbSelected = 0;
+                for (AudioRecord record : records) {
+                    if (record.isChecked()) {
+                        nbSelected++;
+                    }
+                }
+
+                switch (nbSelected) {
+                    case 0:
+                        disableRename();
+                        disableDelete();
+                        break;
+                    case 1:
+                        enableDelete();
+                        enableRename();
+                        break;
+                    default:
+                        disableRename();
+                        enableDelete();
+                }
+
             } else {
                 Intent intent = new Intent(this, AudioPlayerActivity.class);
                 intent.putExtra("filepath", audioRecord.getFilePath());
@@ -200,6 +341,8 @@ public class GalleryActivity extends AppCompatActivity implements OnItemClickLis
                 actionBar.setDisplayShowHomeEnabled(false);
             }
             editbar.setVisibility(View.VISIBLE);
+            enableDelete();
+            enableRename();
         }
     }
 }
